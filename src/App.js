3f9bfc82e867b775058a9703260e2e470b8291f3
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-02-23 16:02:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
 
 function App() {
+  console.log(process.env.REACT_APP_API_ENDPOINT)
+
+  useEffect(() => {
+    fetch(process.res)
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Hey class
         </p>
         <a
           className="App-link"
